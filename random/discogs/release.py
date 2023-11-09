366f268d7bba84a8fef05ab24a8bd2989ace0d56
Istvan Szukacs <istvan@datadeft.eu>
2023-11-09 09:53:33 +0100

    Working release converter

--- random/discogs/release.py
+++ random/discogs/release.py
@@ -1,17 +1,40 @@
 from lxml import etree
+import csv
 
 file_name = "data/discogs_20231001_releases.xml"
-file_name = "data/sample.xml"
+# file_name = "data/sample.xml"
+
+
+def handle_artists(artists):
+    ret = []
+    for artist in artists:
+        artist_dict = {}
+        for artist_tag in artist:
+            if artist_tag.tag == "id":
+                artist_dict["id"] = artist_tag.text
+            elif artist_tag.tag == "name":
+                artist_dict["name"] = artist_tag.text
+        ret.append(artist_dict)
+    return ret
+
 
 events = ("start", "end")
 context = etree.iterparse(file_name, events=events)
 
+with open("data/discogs_20231001_releases.csv", "w") as f:
+    w = csv.DictWriter(f=f, fieldnames=["release_id", "release_title"], delimiter="\t")
+    w.writeheader()
 
-for action, elem in context:
-    if action == "start" and elem.tag == "release":
-        release_id = elem.get("id")
-        for child in elem:
-            if child.tag in ("title"):
-                print(f"BINGO: {release_id} :: {child.text}")
-    else:
-        print(f".")
+    for action, elem in context:
+        if action == "start" and elem.tag == "release":
+            release_id = elem.get("id")
+            release = {"release_id": release_id}
+            for child in elem:
+                tag = child.tag
+                if tag == "title":
+                    release["release_title"] = child.text
+                elif tag == "artists":
+                    # release["artists"] = handle_artists(child)
+                    pass
+            if release.get("release_title") != None:
+                w.writerow(release)
